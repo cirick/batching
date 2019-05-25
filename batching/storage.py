91475f354ae4cbc0ee8beaa14f0bbe045f55cd3f
Charles <cirick@gmail.com>
2019-05-24 21:18:32 -0600

    add reqs for pypi release

--- batching/storage.py
+++ batching/storage.py
@@ -1,6 +1,6 @@
 import os
 from abc import ABC, abstractmethod
-
+import datetime
 import numpy as np
 
 
--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup
 setup(
     name="batching",
     version="0.0.1",
-    description="",
-    url="",
+    description="Batching is a set of tools to format data for training sequence models",
+    url="https://github.com/cirick",
+    download_url="https://github.com/cirick/batching/archive/v0.0.1.tar.gz",
     author="Charles Irick",
-    author_email="",
+    author_email="cirick@gmail.com",
     include_package_data=True,
     license="MIT",
     packages=["batching"],
     install_requires=[
+        "numpy>=1.16.3",
+        "pandas>=0.24.2",
+        "scikit-learn>=0.21.2",
+        "tensorflow>=1.13.1",
     ],
     zip_safe=False,
 )
-
