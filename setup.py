7f8e9157109ef00fce663e55aa6784b0b9dda83b
Charles <cirick@gmail.com>
2019-05-28 13:10:52 -0400

    add ci

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="batching",
     version="0.0.2",
     description="Batching is a set of tools to format data for training sequence models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/cirick/batching",
     download_url="https://github.com/cirick/batching/archive/v0.0.2.tar.gz",
     author="Charles Irick",
